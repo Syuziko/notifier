7da8bad6fc2583957c71686697ea457daa7186c9
syuzanna.eprikyan <syuzanna.eprikyan@sflpro.com>
2019-05-06 18:37:50 +0400

    some refactoring

--- core/core-test-toolkit/src/main/java/com/sflpro/notifier/db/PostgreSQLTestContainerInitializer.java
+++ core/core-test-toolkit/src/main/java/com/sflpro/notifier/db/PostgreSQLTestContainerInitializer.java
@@ -1,12 +1,10 @@
-package com.sflpro.notifier.db.initializer;
+package com.sflpro.notifier.db;
 
 import org.junit.ClassRule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
@@ -20,21 +18,10 @@ import java.util.Properties;
  */
 @Lazy(false)
 @Configuration
-public class DbInitializer {
+public class PostgreSQLTestContainerInitializer {
 
     @ClassRule
     private PostgreSQLContainer postgresContainer = new PostgreSQLContainer("postgres:9.6.8");
-//    private MySQLContainer postgresContainer = new MySQLContainer("mysql:5.6.44");
-
-    @Autowired
-    private JpaVendorAdapter jpaVendorAdapter;
-
-//    static {
-//        postgresContainer = ;
-//        Runtime.getRuntime().addShutdownHook(new Thread(postgresContainer::stop));
-////        postgresContainer.start();
-//
-//    }
 
     @Bean(name = "datasource")
     public DataSource dataSource() {
@@ -52,27 +39,15 @@ public class DbInitializer {
         return dataSource;
     }
 
-//
-//    @Bean(name = "entityManagerFactory")
-//    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean() {
-//        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-//        entityManagerFactoryBean.setDataSource(dataSource());
-////        entityManagerFactoryBean.setPackagesToScan("digital.weadapt.core.db");
-//        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-//        entityManagerFactoryBean.setJpaProperties(getProperties());
-//        return entityManagerFactoryBean;
-//    }
-
     private Properties getProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        properties.setProperty("hibernate.dialect", "\torg.hibernate.dialect.PostgreSQLDialect");
         properties.setProperty("hibernate.show_sql", "false");
         properties.setProperty("hibernate.hbm2ddl.auto", "none");
         properties.setProperty("hibernate.globally_quoted_identifiers", "true");
         properties.setProperty("hibernate.use_second_level_cache", "false");
         properties.setProperty("hibernate.javax.cache.provider", "org.ehcache.jsr107.EhcacheCachingProvider");
         properties.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.jcache.JCacheRegionFactory");
-
         return properties;
     }
 }
